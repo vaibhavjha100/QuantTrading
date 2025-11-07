f1e2e0bd789ea1aa87d7c8a4a8cc4d143141bbf4
Vaibhav Jha <vaibhavjha100@gmail.com>
2025-11-07 17:20:18 +1100

    refactor: shift ExampleStrategy to strategies directory

--- strategy.py
+++ strategy.py
@@ -891,29 +891,7 @@ class BaseStrategy:
         return loaded_strategy
 
 
-class ExampleStrategy(BaseStrategy):
-    """
-    Example implementation of a trading strategy.
-    Inherits from BaseStrategy and implements a random trade signal generator.
-    """
 
-    def get_trade_signal(self, df):
-        """
-        Get trade signal based on random strategy.
-
-        Parameters:
-        df (pd.DataFrame): DataFrame slice for a specific ticker up to the current date.
-
-        Returns:
-        str: Trade signal ('BUY', 'SELL', 'HOLD').
-        """
-
-        if len(df) < 2:
-            return 'HOLD'
-
-        signal = random.choice(['BUY', 'SELL', 'HOLD'])
-
-        return signal
 
 
 
